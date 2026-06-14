0a344fd213d8c915cf16e8c1dc7bc546539167b0
Connie AI <connie.ai@coseeing.org>
2026-06-14 11:53:36 +0000

    feat: add keyboard pipeline result types

--- src/application/input/__init__.py
+++ src/application/input/__init__.py
@@ -1,9 +1,12 @@
 from application.input.activation import InputActivationUseCase
 from application.input.active_key_policy import ActiveKeyEventPolicy
+from application.input.results import AppKeyEventResult, KeyboardPipelineResult
 from application.input.system_toggle_policy import should_pass_through_system_toggle
 
 __all__ = [
     "ActiveKeyEventPolicy",
+    "AppKeyEventResult",
     "InputActivationUseCase",
+    "KeyboardPipelineResult",
     "should_pass_through_system_toggle",
 ]
