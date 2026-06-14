8dd724f0e7b8267bb73e32cafdcbd2636c6ef579
Connie AI <connie.ai@coseeing.org>
2026-06-14 11:55:57 +0000

    feat: add keyboard pipeline assembly helper

--- src/application/input/__init__.py
+++ src/application/input/__init__.py
@@ -1,11 +1,13 @@
 from application.input.activation import InputActivationUseCase
 from application.input.active_key_policy import ActiveKeyEventPolicy
+from application.input.keyboard_pipeline import assemble_pipeline_result
 from application.input.results import AppKeyEventResult, KeyboardPipelineResult
 from application.input.system_toggle_policy import should_pass_through_system_toggle
 
 __all__ = [
     "ActiveKeyEventPolicy",
     "AppKeyEventResult",
+    "assemble_pipeline_result",
     "InputActivationUseCase",
     "KeyboardPipelineResult",
     "should_pass_through_system_toggle",
